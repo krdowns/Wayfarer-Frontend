e3ca314e60eb90c6026de32fc73f23004fe38461
krdowns <krjdowns16@gmail.com>
2019-01-10 14:58:07 -0800

    connected to backend

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,18 @@ class About extends Component {
   render () {
     return (
         <div>
-            <h1>About</h1>
+            <h1>Wayfarer is...</h1>
+            <section>
+              <article>
+                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+              </article>
+              <article>
+                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+              </article>
+              <article>
+                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+              </article>
+            </section>
         </div>
     )
   }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,32 +4,19 @@ import './NavBar.css'
 
 class NavBar extends Component {
   render () {
-
+    let navBarItems = [<NavItem key={1} href='/'>Cities</NavItem>]
+    if (this.props.isLoggedIn) {
+      navBarItems.push(<NavItem key={2} href='/logout'>Log Out</NavItem>)
+    } else {
+      navBarItems.push(<NavItem key={3} href='/signup'>Sign Up</NavItem>)
+      navBarItems.push(<NavItem key={4} href='/login'>Log In</NavItem>)
+    }
     return (
-      <nav>
-        <ul className="navbarlogo">
-          <li>Wayfarer</li>
-        </ul>
-        <ul className="navbarbuttons">
-          <li>Login</li>
-          <li>Sign Up</li>
-        </ul>
-      </nav>
-
+      <Navbar brand='Wayfarer' className='nav' right>
+        {navBarItems}
+      </Navbar>
     )
   }
 }
 
 export default NavBar
-
-// let navBarItems = [<NavItem key={1} href='/'>Cities</NavItem>]
-// if (this.props.isLoggedIn) {
-//   navBarItems.push(<NavItem key={2} href='/logout'>Log Out</NavItem>)
-// } else {
-//   navBarItems.push(<NavItem key={3} href='/signup'>Sign Up</NavItem>)
-//   navBarItems.push(<NavItem key={4} href='/login'>Log In</NavItem>)
-// }
-
-// <Navbar brand='Wayfarer' className='nav' right>
-// {navBarItems}
-// </Navbar>
