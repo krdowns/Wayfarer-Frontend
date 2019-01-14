e9c98c1438b7d73c2866cc0cedeca9d1eeb64b8a
krdowns <krjdowns16@gmail.com>
2019-01-14 12:10:40 -0800

    updated styling and created recent user post component

--- src/components/ProfileDetails/ProfileDetails.js
+++ src/components/ProfileDetails/ProfileDetails.js
@@ -6,13 +6,11 @@ class ProfileDetails extends Component {
     return (
       <div className="profile-page-left">
         <img className="profile-pic" src="../images/dark-profile-default.jpg" alt="profile-pic" />
+        <button class="edit-profile-button">Edit Profile</button>
         <div className="user-info">
-          <h6>Kenny Downs</h6>
-            <br/>
-          <h6>City: San Francisco 
-            <br/>
-            <br/>Joined: 01/10/19 
-          </h6>
+          <h6 className="user-name">Kenny Downs</h6>
+          <h6 className="current-city">City: San Francisco</h6>
+          <h6 className="join-date">Joined: 01/10/19 </h6>
         </div>
       </div>
     )
