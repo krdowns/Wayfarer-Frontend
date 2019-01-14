09e661afb33a0286a56b1083ac3798f07f1987ed
krdowns <krjdowns16@gmail.com>
2019-01-14 15:08:19 -0800

    updated about styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,16 +5,27 @@ class About extends Component {
   render () {
     return (
         <div>
-            <h1 className="about-header">Wayfarer is...</h1>
-            <section>
+            <header className="about-header">Wayfarer is...</header>
+            <section className="about-text">
               <article>
-                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+                <h3>Urban</h3>
+                <p>
+                  You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.
+                </p>
               </article>
+              <div className="about-dividing-line"></div>
               <article>
-                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+                <h3>Travel</h3>
+                <p>
+                  You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.
+                </p>
               </article>
+              <div className="about-dividing-line"></div>
               <article>
-                <p>You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.</p>
+                <h3>Stories</h3>
+                <p>
+                  You're meant to have fun in life. Get away from those little Christmas tree things we used to make in school. All you need is a dream in your heart, and an almighty knife. You need the dark in order to show the light. I spend a lot of time walking around in the woods and talking to trees, and squirrels, and little rabbits and stuff.
+                </p>
               </article>
             </section>
         </div>
