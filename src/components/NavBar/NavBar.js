759427a846eb5508d2acdee748b39669e5c2018f
jbortiz0623 <jbortiz0623@gmail.com>
2019-01-14 11:21:41 -0800

    inserted profile page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
+import {Modal, Button} from 'react-materialize'
 import './NavBar.css'
 
 class NavBar extends Component {
   render () {    
     return (
-
       <div className="navbar-fixed">
         <nav>
             <div className="nav-wrapper">
-            <a href="#" className="brand-logo left">Wayfarer</a>
-            <ul>
-              <li><a href="/">Sign Up</a></li>
-              <li><a href="/">Login</a></li>
-            </ul>
+            <a href="/" className="brand-logo left">Wayfarer</a>
+            
+              <Modal
+              header='Modal Header'
+              trigger={<Button waves='light'>Sign Up</Button>}>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.</p>
+              </Modal>
+              
+            
+              <Modal header="Login!"
+              trigger={<Button waves="yellow">Login</Button>}>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.</p>
+              </Modal>
+              
+
             </div>
           </nav>
       </div>
--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -4,9 +4,16 @@ import './ProfilePage.css'
 class ProfilePage extends Component {
   render () {
     return (
-        <div>
-            <h1>ProfilePage</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            1 of 
+          </div>
+          <div className="col">
+            2 of
+          </div>
         </div>
+      </div>
     )
   }
 }
