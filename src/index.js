54d381c899b2cb3caa47616fbb771ed447d2aa23
krdowns <krjdowns16@gmail.com>
2019-01-16 13:57:51 -0800

    cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-app.use(function (req, res, next) {
+App.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
